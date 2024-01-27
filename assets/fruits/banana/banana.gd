7ec3dce2098a2e34a8edb479e6c4b5f9a20c5d08
jxarco <u114145@upf.edu>
2024-01-27 13:08:59 +0100

    add block functionality (simulating On_hit by now)

--- assets/fruits/banana/banana.gd
+++ assets/fruits/banana/banana.gd
@@ -3,3 +3,7 @@ extends "res://assets/fruits/controller.gd"
 func _physics_process(delta):
 
 	process_movement(delta, "JUMP_2", ["MOVE_LEFT_2", "MOVE_RIGHT_2"])
+
+func _input(event):
+	
+	process_moves(event, "BLOCK_2")
