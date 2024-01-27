0ac605ecb8914a1ab5f1f5ac582f9309a5a2a54e
PZerua <pachus.wtf@gmail.com>
2024-01-27 13:34:01 +0100

    wip colliders

--- assets/fruits/tomato/tomato.gd
+++ assets/fruits/tomato/tomato.gd
@@ -1,5 +1,10 @@
 extends "res://assets/fruits/controller.gd"
 
+func _process(delta):
+	
+	if Input.is_action_just_pressed("PUNCH_1"):
+		print("punch")
+
 func _physics_process(delta):
 
 	process_movement(delta, "JUMP_1", ["MOVE_LEFT_1", "MOVE_RIGHT_1"])
