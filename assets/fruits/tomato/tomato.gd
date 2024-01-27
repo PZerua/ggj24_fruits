e1511ddb242fae34d071b33367a0c1429d472551
jxarco <alexroco.30@gmail.com>
2024-01-27 13:27:57 +0100

    wip more controls, prepare flip texture

--- assets/fruits/tomato/tomato.gd
+++ assets/fruits/tomato/tomato.gd
@@ -1,9 +1,14 @@
 extends "res://assets/fruits/controller.gd"
 
+func _process(delta):
+	
+	var mat : StandardMaterial3D = %Sprite2D.get_surface_override_material(0)
+	flip_uv_if_necessary(mat, "", "")
+
 func _physics_process(delta):
 
 	process_movement(delta, "JUMP_1", ["MOVE_LEFT_1", "MOVE_RIGHT_1"])
 
 func _input(event):
 	
-	process_moves("BLOCK_1")
+	process_moves(["PUNCH_1", "KICK_1", "BLOCK_1"])
