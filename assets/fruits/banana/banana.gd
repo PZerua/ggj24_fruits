125e7ce11a5ab785ba2de1683cdd83cc712d6b9a
jxarco <alexroco.30@gmail.com>
2024-01-28 10:57:38 +0100

    process_moves each frame

--- assets/fruits/banana/banana.gd
+++ assets/fruits/banana/banana.gd
@@ -10,13 +10,13 @@ func _ready():
 	look_left()
 	
 func _process(delta):
+	process_moves(["PUNCH_2", "KICK_2", "BLOCK_2", "MOVE_LEFT_2", "MOVE_RIGHT_2"])
 	super._process(delta)
 
 func _physics_process(delta):
 	process_movement(delta, "JUMP_2", ["MOVE_LEFT_2", "MOVE_RIGHT_2"])
 
-func _input(event):
-	process_moves(["PUNCH_2", "KICK_2", "BLOCK_2", "MOVE_LEFT_2", "MOVE_RIGHT_2"])
+# func _input(event):
 
 func _on_punch_trigger_body_entered(body):
 	process_hit(body, punch_damage)
@@ -27,7 +27,7 @@ func _on_kick_trigger_body_entered(body):
 func _on_animated_sprite_2d_animation_finished():
 	if ($AnimatedSprite2D.animation == "punch") or \
 		($AnimatedSprite2D.animation == "kick"):
-		attack_charge = 0
+		attack_charge = 0.0
 
 func _on_animated_sprite_2d_frame_changed():
 	if $AnimatedSprite2D.animation == "punch":
