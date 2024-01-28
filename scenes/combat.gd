bea259d172e6e37aa9d628a7338431b9d2742cbc
jxarco <alexroco.30@gmail.com>
2024-01-28 17:09:54 +0100

    credits ok

--- scenes/combat.gd
+++ scenes/combat.gd
@@ -42,4 +42,4 @@ func _on_timer_timeout():
 	
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fade_out":
-		get_tree().change_scene_to_file("res://scenes/end_game.tscn")
+		get_tree().change_scene_to_file("res://scenes/credits.tscn")
