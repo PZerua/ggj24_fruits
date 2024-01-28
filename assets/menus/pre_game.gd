526a1d26776eaed2d3476d9f89e4e4d8045c4d04
jxarco <alexroco.30@gmail.com>
2024-01-28 15:59:41 +0100

    Add grape cinematic audio

--- assets/menus/pre_game.gd
+++ assets/menus/pre_game.gd
@@ -12,7 +12,7 @@ const lines = [
 
 func _ready():
 	$Label.text = lines[current_line]
-
+	
 func _process(delta):
 	timer -= delta
 	if timer <= 0.0 or Input.is_action_pressed("PUNCH_1") or Input.is_action_pressed("PUNCH_2"):
@@ -21,6 +21,15 @@ func _process(delta):
 			print("Combat Started. Fight!")
 			get_tree().change_scene_to_file("res://scenes/combat.tscn")
 			return
+		if current_line == 1:
+			$AudioStreamPlayer2D.stream = load("res://assets/audio/grape_cinematic_med.ogg")
+			timer = 4.0
+		if current_line == 2:
+			$AudioStreamPlayer2D.stream = load("res://assets/audio/grape_cinematic.ogg")
+			timer = 6.0
+		if current_line == 3:
+			$AudioStreamPlayer2D.stream = load("res://assets/audio/grape_cinematic_short.ogg")
+			timer = 3.0
+		$AudioStreamPlayer2D.play()
 		$Background.texture = load("res://assets/menus/Intro_02.png")
 		$Label.text = lines[current_line]
-		timer = 5.0
