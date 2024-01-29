e48b475e4bf6d9822dc5750f234bdd71de332ece
jxarco <alexroco.30@gmail.com>
2024-01-29 16:47:58 +0100

    fix put credits after queen scene

--- scenes/end_game.gd
+++ scenes/end_game.gd
@@ -11,4 +11,4 @@ func _process(delta):
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fade_out":
-		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+		get_tree().change_scene_to_file("res://scenes/credits.tscn")
--- scenes/menu.gd
+++ scenes/menu.gd
@@ -18,4 +18,4 @@ func _on_exit_pressed():
 	get_tree().quit()
 	
 func _on_controls_pressed():
-	get_tree().change_scene_to_file("res://scenes/controls.tscn")
+	get_tree().change_scene_to_file("res://scenes/end_game.tscn")
